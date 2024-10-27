8566b9c5b51ec2780391cff07e232d8e3de186f4
Bobaliciousss <149013858+Bobaliciousss@users.noreply.github.com>
2024-10-27 15:57:29 -0700

    Update Food.jsx
    Adding changes

--- src/client/components/Food.jsx
+++ src/client/components/Food.jsx
@@ -2,11 +2,21 @@
 
 // Will return a "container" for the header of the website
 export default function Food( props ) {
+
+
+    // The handleDelete function calls a function to delete an expense
+    const handleClick = () => {
+      props.deleteFood( props.index );
+    }
+
+
+
     return (
 
             <div className = "food">
               <h1>{ props.name }</h1>
               <p>{ props.calories }</p>
+              <button onClick = { handleClick }>Delete</button>
             </div>
 
     )
