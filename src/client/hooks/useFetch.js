f8003aa4bda1082d22ea43ffb7323f5491d1bff3
Alonso The Dopest <128670706+abg287@users.noreply.github.com>
2024-10-27 21:33:15 -0700

    Update useFetch.js

--- src/client/hooks/useFetch.js
+++ src/client/hooks/useFetch.js
@@ -2,12 +2,16 @@ import {useEffect, useState} from 'react';
 
 export default function useFetch(path) {
   const [data, setData] = useState();
+
+  const HOST = process.env.SERVER_HOST || "localhost";
+  const PORT = process.env.SERVER_PORT || "8080";
+  
   useEffect(() => {
     // Cancel the fetch request
     const controller = new AbortController();
     const {signal} = controller;
 
-    fetch(`http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}${path}`, {signal})
+    fetch(`http://${HOST}:${PORT}${path}`, {signal})
       .then( resp => resp.json() )
       .then( data => {setData( data ) }) 
       .catch( err => console.error(err));
